98d2f90f11fa30b0b5d68b0be7fc9be49208d804
yukechen369 <750295736@qq.com>
2021-12-07 16:26:29 +0800

    测试了基于时间戳的负载均衡策略，配置写死在了server.go中的NewProxyServer

--- pkg/proxyServer/server.go
+++ pkg/proxyServer/server.go
@@ -11,13 +11,29 @@ import (
 )
 
 type ProxyServer struct {
-	Host     string
+	Host     string //监听地址和端口
 	Port     string
 	Balancer loadBalancer.Balancer
 }
 
 func NewProxyServer(*config.Config) *ProxyServer {
-	return nil
+	s1 := loadBalancer.Server{
+		Host: "127.0.0.1",
+		Port: "8001",
+	}
+	s2 := loadBalancer.Server{
+		Host: "127.0.0.1",
+		Port: "8002",
+	}
+	var serverAr = []*loadBalancer.Server{&s1, &s2}
+	ser := ProxyServer{
+		Host: "0.0.0.0",
+		Port: "8888",
+		Balancer: loadBalancer.TimeStampRandomBalancer{
+			Servers: serverAr,
+		},
+	}
+	return &ser
 }
 
 func (s *ProxyServer) StartServer() {
@@ -25,19 +41,23 @@ func (s *ProxyServer) StartServer() {
 		Addr:    s.Host + ":" + s.Port,
 		Handler: s,
 	}
+	log.Printf("Listening on %s", server.Addr)
 	server.ListenAndServe()
 }
 
 func (s *ProxyServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println(r.RemoteAddr + " " + r.Method + " " + r.URL.String() + " " + r.Proto + " " + r.UserAgent())
+	//获取负载均衡地址
 	remoteServer, err := s.Balancer.DoBalance()
 	if err != nil {
 		log.Println("")
 	}
-	remoteURL, err := url.Parse(s.Host + ":" + s.Port)
+	remoteURL, err := url.Parse("http://" + remoteServer.Host + ":" + remoteServer.Port)
 	if err != nil {
-		log.Println("")
+		log.Fatalln(err)
 	}
+	log.Printf("forward to: " + remoteURL.Host)
 	proxy := httputil.NewSingleHostReverseProxy(remoteURL)
-	r.Host = remoteServer.Host
+	r.Host = remoteServer.Host + ":" + remoteServer.Port
 	proxy.ServeHTTP(w, r)
 }
