40636514c4bf8891375a56096b2a5505ce25fd70
lwhere <624276736@qq.com>
2021-12-09 10:41:44 +0800

    json read completed

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,10 +1,38 @@
 package config
 
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type Proxyserver struct {
+	Ip        string
+	Port      string
+	Algorithm string
+}
+type Server struct {
+	Ip   string
+	Port string
+}
+
 type Config struct {
+	Nginx   []Proxyserver
+	Servers []Server
 }
 
 func LoadConfig(configFile string) (*Config, error) {
 
-	//test
-	return nil, nil
+	jsonFile, err := os.ReadFile(configFile)
+	if err != nil {
+		log.Fatalln(err)
+		return nil, err
+	}
+	config := new(Config)
+	err = json.Unmarshal(jsonFile, config)
+	if err != nil {
+		log.Fatalln(err)
+		return nil, err
+	}
+	return config, nil
 }
---
+++ pkg/config/config_test.go
@@ -0,0 +1,17 @@
+package config
+
+import (
+	"log"
+	"testing"
+)
+
+// import "testing"
+
+// func TestLoadConfig(t *testing.T) {
+// 	configFile := "../config/config.json"
+// 	LoadConfig(configFile)
+// }
+
+func TestLoadConfig(t *testing.T) {
+	log.Print("")
+}
